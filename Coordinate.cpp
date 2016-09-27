2958b48386de181c3071aaa8a24e7b66eefd5994
blueskymoon <yueliangdeshouhuzhe@outlook.com>
2016-09-27 12:18:36 +0800

    Add files via upload
    对象成员指针

---
+++ Coordinate.cpp
@@ -0,0 +1,22 @@
+
+//***********************************************
+//      Filename: Coordinate.cpp
+//        Author:  liuyuyuan 
+//        Create: 2016-09-25 21:16:33
+// Last Modified: 2016-09-25 21:16:33
+//***********************************************
+
+
+#include <iostream>
+#include "Coordinate.h"
+using namespace std;
+
+Coordinat::Coordinate()
+{
+    cout<<"Coordinate"<<endl;
+}
+
+Coordinate::~Coordinate()
+{
+    cout<<"~Coordinate"<<endl;
+}
